2cf88f6dcff5e7083cb680ec068adb219bee263f
Abir Hasan <cse.170201063@gmail.com>
2023-05-13 01:27:38 +0600

    default export

--- Milestone1/module2/src/module5.5/main.ts
+++ Milestone1/module2/src/module5.5/main.ts
@@ -1,9 +1,12 @@
 // import { add as addtwo, subtract, multiply } from './module'   // evabe korbo jodi module er moddhe akta akta kore export kora thake.
-import * as methods from './module' // akbare sob chole ashlo. eke bole wild card export
-
+// import * as methods from './module' // akbare sob chole ashlo. eke bole wild card export
+// import average from './module'  // default vabe export korar karone object akare import korte hocche na. avarage na dia jodi onno kono nam o dei ami. taw oi function kei indicate korbe. jehetu akhn only average export kora ase.
+import jekono from './module'
 const add = (param1: number, param2: number): number => {
     return param1 + param2
 }
 
 // const ok= addtwo(1,2) // for 1
-const ok2= methods.add(1, 2) // for 2
+// const ok2 = methods.add(1, 2) // for 2
+// const ok3 = average(2, 2); // for 3
+const ok4 = jekono.average(2, 3);
--- Milestone1/module2/src/module5.5/module.ts
+++ Milestone1/module2/src/module5.5/module.ts
@@ -1,21 +1,32 @@
-export const add = (param1: number, param2: number): number => {
-    return param1 + param2;
-}
+// export
+const add = (param1: number, param2: number): number => {
+  return param1 + param2;
+};
 
-export const subtract = (param1: number, param2: number): number => {
-    return param1 - param2;
-}
+// export
+const subtract = (param1: number, param2: number): number => {
+  return param1 - param2;
+};
 
-export const multiply = (param1: number, param2: number): number => {
-    return param1 * param2;
-}
+// export
+const multiply = (param1: number, param2: number): number => {
+  return param1 * param2;
+};
 
-export const divide = (param1: number, param2: number): number => {
-    return param1 / param2;
-}
+// export
+const divide = (param1: number, param2: number): number => {
+  return param1 / param2;
+};
 
 const average = (param1: number, param2: number): number => {
-    return param1 + param2 / 2; 
-}
- 
-export default average
+  return param1 + param2 / 2;
+};
+
+// sob export default korbo tai line guloi sob export hide kore disi
+export default {
+  average,
+  divide,
+  multiply,
+  add,
+  subtract,
+};
